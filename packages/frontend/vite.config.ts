dc758dbef93810829a4f560a313f948427789651
Mashiro Wang <github@te.sb>
2023-12-11 21:15:56 +0800

    fix: only enable when dev mode

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -3,31 +3,33 @@ import vue from '@vitejs/plugin-vue';
 import * as path from 'path';
 import UnoCSS from 'unocss/vite';
 import Components from 'unplugin-vue-components/vite';
-import { defineConfig } from 'vite';
+import { ConfigEnv, UserConfigExport } from 'vite';
 import { viteVConsole } from 'vite-plugin-vconsole';
 
-export default defineConfig({
-  plugins: [
-    vue(),
-    Components({
-      resolvers: [VantResolver()],
-    }),
-    UnoCSS(),
-    viteVConsole({
-      entry: [path.resolve('src/main.ts')], // entry for each page, different from the above
-      enabled: true,
-      config: {
-        maxLogNumber: 1000,
-        theme: 'dark',
-      },
-    }),
-  ],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
+export default ({ mode }: ConfigEnv): UserConfigExport => {
+  return {
+    plugins: [
+      vue(),
+      Components({
+        resolvers: [VantResolver()],
+      }),
+      UnoCSS(),
+      viteVConsole({
+        entry: [path.resolve('src/main.ts')], // entry for each page, different from the above
+        enabled: mode !== 'production',
+        config: {
+          maxLogNumber: 1000,
+          theme: 'dark',
+        },
+      }),
+    ],
+    server: {
+      proxy: {
+        '/api': {
+          target: 'http://localhost:3000',
+          changeOrigin: true,
+        },
       },
     },
-  },
-});
+  };
+};
