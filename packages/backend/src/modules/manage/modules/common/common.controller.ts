f728d9ad58b6fc98fbe535244b4289ec6b72e047
Mashiro Wang <github@te.sb>
2023-12-11 21:11:46 +0800

    fix: set env private

--- packages/backend/src/modules/manage/modules/common/common.controller.ts
+++ packages/backend/src/modules/manage/modules/common/common.controller.ts
@@ -1,12 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
 
 import packageJson from '../../../../../package.json';
-import { Public } from '../../../auth/auth.decorator';
 
 @Controller('common')
 export class CommonController {
   @Get('env')
-  @Public()
   getEnv() {
     return {
       version: packageJson.version,
